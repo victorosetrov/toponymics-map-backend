777fdb4ad1780ab3dd971997147f3075b059d654
Lab 1 <victorosetrov@gmail.com>
2022-12-08 09:38:47 +0300

    updated port

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.sgjfa.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(9000);
+    app.listen(process.env.PORT || 9000);
   })
   .catch(err => {
     console.log(err);
