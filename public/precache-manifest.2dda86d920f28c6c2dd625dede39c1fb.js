c37867cd4d1df93585a214a518043813d8f1f364
Lab 1 <victorosetrov@gmail.com>
2022-12-08 11:40:27 +0300

    Added new react app frontend

---
+++ public/precache-manifest.2dda86d920f28c6c2dd625dede39c1fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ed9ae6c51560dc5adc22333099ed2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240dfe075f07c18948cf",
+    "url": "/static/css/main.5c17acf9.chunk.css"
+  },
+  {
+    "revision": "a3d5e67de3769ff4b4cb",
+    "url": "/static/js/2.c89bff58.chunk.js"
+  },
+  {
+    "revision": "240dfe075f07c18948cf",
+    "url": "/static/js/main.0e15bf67.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
